297927d0aba7eae1ca02e2c1c42c8271d96ca8c0
Igor Krupkin <krupkin@echo-company.ru>
2020-12-18 00:35:00 +0500

    add bipit9

---
+++ BIPIT09/BIPIT09/Controllers/PagesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIPIT09.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View("Clients");
+        }
+
+        public ActionResult Services()
+        {
+            return View("Services");
+        }
+        public ActionResult Shippings()
+        {
+            return View("Shippings");
+        }
+
+    }
+}
---
+++ BIPIT09/BIPIT09/Controllers/TableController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BIPIT09.Models;
+
+namespace BIPIT09.Controllers
+{
+    public class TableController : Controller
+    {
+        bipitpjEntities db = new bipitpjEntities();
+        // GET: Table
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult ShippingsPartial()
+        {
+            var shoppingsViews = db.View_3.ToList();
+
+            return PartialView(shoppingsViews);
+        }
+
+        public PartialViewResult ClientsPartial()
+        {
+            var clientsViews = db.Clients.ToList();
+
+            return PartialView(clientsViews);
+        }
+
+        public PartialViewResult ServicesPartial()
+        {
+            var servicesViews = db.Services.ToList();
+
+            return PartialView(servicesViews);
+        }
+    }
+}
